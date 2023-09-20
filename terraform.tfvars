16ccce79fe5219889a63134760a61a4b209fc8d6
rana-sahil-221 <sahil2000.sr@gmail.com>
2023-09-20 12:31:54 +0530

    Initial commit

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+access_key = "AKIA3KKAGQZBBYDYUM5A"
+secret_key = "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"
---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "access_key" {
+        description = "Access key to AWS console"
+}
+variable "secret_key" {
+        description = "Secret key to AWS console"
+}
+
+
+variable "instance_name" {
+        description = "Name of the instance to be created"
+        default = "infra-demo"
+}
+
+variable "instance_type" {
+        default = "t2.medium"
+}
+
+variable "subnet_id" {
+        description = "The VPC subnet the instance(s) will be created in"
+        default = "subnet-004775540ec2d9dae"
+}
+
+variable "ami_id" {
+        description = "The AMI to use"
+        default = "ami-04cb4ca688797756f"
+}
+
+variable "number_of_instances" {
+        description = "number of instances to be created"
+        default = 1
+}
+
+
+variable "ami_key_pair_name" {
+        default = "infratest"
+}
